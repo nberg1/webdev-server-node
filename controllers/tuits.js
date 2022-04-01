4471b62635a0351df24cc94535d345e686758650
nberg002 <berg.n@northeastern.edu>
2022-03-31 22:44:33 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        "_id": "123",
+        "postedBy": {"username": "NASA"},
+        "userName": "NASA",
+        "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+        "title": "In 2021, our @NASAPersevere Mars rover landed",
+        "Like": 345,
+        "Dislike": 0
+    },
+    {
+        "_id": "234",
+        "postedBy": { "username": "NASA" },
+        "userName" : "NASA",
+        "tuit": "Ingenuity helicopter takes flight.",
+        "title": "Ingenuity helicopter takes flight.",
+        "Like": 346785,
+        "Dislike": 25
+    }
+];
