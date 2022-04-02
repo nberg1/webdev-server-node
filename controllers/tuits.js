4a8c27359bd2f73e026e157bbbe1c8ec3ddce820
nberg002 <berg.n@northeastern.edu>
2022-04-02 15:32:45 -0400

    a8 modifying tuits data to match

--- controllers/tuits.js
+++ controllers/tuits.js
@@ -3,6 +3,8 @@ export default [
         "_id": "123",
         "postedBy": {"username": "NASA"},
         "userName": "NASA",
+        "User": "NASA",
+        "UserImage": "/images/nasa_logo.png",
         "tuit": "In 2021, our @NASAPersevere Mars rover landed",
         "title": "In 2021, our @NASAPersevere Mars rover landed",
         "Like": 345,
@@ -11,7 +13,9 @@ export default [
     {
         "_id": "234",
         "postedBy": { "username": "NASA" },
+        "User": "NASA",
         "userName" : "NASA",
+        "UserImage": "/images/nasa_logo.png",
         "tuit": "Ingenuity helicopter takes flight.",
         "title": "Ingenuity helicopter takes flight.",
         "Like": 346785,
